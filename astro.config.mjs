ef7e234523c6dcc1f1d5aa6b33a5f7d41dff53a3
Nick-Job <Nick_Job@163.com>
2025-01-04 00:46:56 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://nick-job.github.io', // 修正 URL
+  base: '/Astro-Nick/', // 替换为你的仓库名称
+  integrations: [mdx(), sitemap()],
 });
